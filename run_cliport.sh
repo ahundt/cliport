30e6c154641c750855d026105d81e35e0556ba18
willie <wagnew3@cs.washington.edu>
2022-04-21 15:53:33 -0700

    person block

--- run_cliport.sh
+++ run_cliport.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=ckpt
 #SBATCH --account=cse
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --gres=gpu:0
+#SBATCH --gres=gpu:1
 #SBATCH --time=10:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=wagnew3@uw.edu
